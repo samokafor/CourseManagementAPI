8ed74ad079aa5fcf8b7aacaae754a7d762a6d49d
Samuel Okafor <samuelvokafor@gmail.com>
2024-04-22 09:59:17 +0100

    Bug fixes on course entity

--- DTOs/CourseDto.cs
+++ DTOs/CourseDto.cs
@@ -7,17 +7,5 @@
         public double CourseDuration { get; set; } //This should be in Days
         public int? InstructorId { get; set; }
         public DateTime StartDate { get; set; }
-
-        public CourseDto()
-        {
-            if (Name == null)
-            {
-                Name = "";
-            }
-            if (Description == null)
-            {
-                Description = "";
-            }
-        }
     }
 }
